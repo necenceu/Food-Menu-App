91496cbdd48be64800e3ec7728584eef1f9f2f2a
Enzh <necenceu@gmail.com>
2023-05-28 03:19:37 +0600

    Managed to made a template of index and detail pages. View for detail. Added a image url to model and migrated it.

--- food/models.py
+++ food/models.py
@@ -10,3 +10,5 @@ class Item(models.Model):
     item_name = models.CharField(max_length=200)
     item_desc = models.CharField(max_length=200)
     item_price = models.IntegerField()
+    item_image_url = models.URLField(max_length=200, default='https://media.istockphoto.com/id/1193057179/vector/photo-coming-soon-image-icon-vector-illustration-isolated-on-white-background-no-website.jpg?s=612x612&w=0&k=20&c=ewySSAlHhs8A97rqdS2sFrvExzs24k70n0uXeNC7peM=')
+    
--- food/views.py
+++ food/views.py
@@ -1,13 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 from .models import Item
 
 # Create your views here.
 
 def index(request):
     item_list = Item.objects.all()
-    return HttpResponse(item_list)
+    main_page_name = 'Good Taste'
+    context = {
+        'item_list':item_list,
+        'main_page':main_page_name,
+    }
+    return render(request, 'food/index.html', context)
+
+def detail(request, item_id):
+    item = Item.objects.get(pk=item_id)
+    context = {
+        'item':item
+    }
+    return render(request, 'food/detail.html', context)
 
 def item(request):
-    return HttpResponse('<h1>This is a item view<h1>')
+    return HttpResponse('This is a item view')
     
