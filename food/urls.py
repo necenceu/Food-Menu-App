3bfa60b4b82c76c57fbf0ad29bb6a690a0b2404f
Enzh <necenceu@gmail.com>
2023-05-29 02:41:34 +0600

    Created a functionality of create update and delete items. Made urls, views and templates for this. Made minor fixes of front end part.

--- food/urls.py
+++ food/urls.py
@@ -4,7 +4,21 @@ from . import views
 app_name = 'food'
 
 urlpatterns = [
+    # main page of the website
     path('', views.index, name='index'),
+
+    # detail of item
     path('<int:item_id>/', views.detail, name='detail'),
+
+    # some item path to test fucntionality
     path('item/', views.item, name='item'),
+
+    # adding item
+    path('add', views.add_item, name='add_item'),
+
+    # editing item
+    path('update/<int:id>/', views.update_item, name='update_item'),
+
+    # deleting item
+    path('delete/<int:id>/', views.delete_item, name='delete_item'), 
 ]
--- food/views.py
+++ food/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template import loader
 from .models import Item
+from .forms import ItemForm
 
 # Create your views here.
 
@@ -22,6 +23,44 @@ def detail(request, item_id):
         'page_name':page_name,
     }
     return render(request, 'food/detail.html', context)
+
+def add_item(request):
+    form = ItemForm(request.POST or None)
+    name = 'Add item'
+    context = {
+        'form':form,
+        'page_name':name,
+    }
+
+    if form.is_valid():
+        form.save()
+        return redirect('food:index')
+    
+    return render(request, 'food/item-form.html', context)
+
+def update_item(request, id):
+    item = Item.objects.get(id = id)
+    form = ItemForm(request.POST or None, instance=item)
+    name = 'Update item'
+    context = {
+        'item':item,
+        'form':form,
+        'page_name':name,
+    }
+
+    if form.is_valid():
+        form.save()
+        return redirect('food:index')    
+    return render(request, 'food/item-form.html', context)
+
+def delete_item(request, id):
+    item = Item.objects.get(id = id)
+
+    if request.method == 'POST':
+        item.delete()
+        return redirect('food:index')
+    
+    return render(request, 'food/item-delete.html', {'item':item})
 
 def item(request):
     return HttpResponse('This is an item view')
